30302b7a33966ebc854016f49f1aac9526b8533a
Anmol Tadikonda <76299725+BoapSoap@users.noreply.github.com>
2026-08-05 14:19:20 -0700

    Add: is-anagram - submission-29

---
+++ Data Structures & Algorithms/is-anagram/submission-29.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap = {}
+
+        for l in s:
+            if l in hashmap: 
+                hashmap[l] += 1
+            if l not in hashmap:
+                hashmap[l] = 1
+        
+        for l2 in t:
+            if l2 not in hashmap:
+                return False
+            if l2 in hashmap and hashmap[l2] == 0:
+                return False
+            if l2 in hashmap and hashmap[l2] != 0: 
+                hashmap[l2] -=1
+        
+        if sum(hashmap.values()) == 0:
+            return True
+        else: 
+            return False
+
+
