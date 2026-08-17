81d4cbafb2bb5f8b52acf3d565401513b54b93c8
Anmol Tadikonda <76299725+BoapSoap@users.noreply.github.com>
2026-08-17 16:48:53 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,46 @@
+import string
+
+class Solution:
+
+    def uniqueValue(self, word: str) -> tuple:
+        alphabet = {}
+
+        for letter in string.ascii_lowercase:
+            alphabet[letter] = 0
+        
+        for letter in word: 
+            if letter in alphabet:
+                alphabet[letter] += 1
+        
+        return tuple(alphabet.values())
+        
+
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        word_bank = {}
+
+        for word in strs:
+            signature = self.uniqueValue(word)
+
+            if signature in word_bank:
+                word_bank[signature].append(word)
+
+            else:
+                word_bank[signature] = [word]
+            
+        
+        # next step is to take the values and  combine them into a single nested list
+
+        return list(word_bank.values())
+
+
+
+
+
+
+                
+
+    
+        
+        
