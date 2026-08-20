356bcb545673f6f1b3d14ae375e65c0bba76a40b
Anmol Tadikonda <76299725+BoapSoap@users.noreply.github.com>
2026-08-19 18:48:47 -0700

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,47 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        hashmap = {}
+
+        freq : list[int] = []
+
+        returnThis : list[int] = []
+
+        for i in range(len(nums)):
+            # Populate hashmap with counts
+            if nums[i] not in hashmap:
+                hashmap[nums[i]] = 1
+            else: 
+                hashmap[nums[i]] += 1
+        
+        for key in hashmap: 
+            freq.append(hashmap[key])
+        
+        freq.sort(reverse=True)   
+
+        # convert to set and slice 
+        target_Frq = set(freq[0:k])
+        
+        for key in hashmap: 
+            if hashmap[key] in target_Frq:
+                returnThis.append(key)
+        
+        return returnThis
+
+            
+
+
+
+            
+
+
+
+# if the value of the current key is greater than the previous key, drop the previous key out of the set dependent on the drop limit, the drop limit is defined as # keys  - k 
+
+# This involves me storing the value of the previous key, in a hashset and comparing it against that. 
+# This is to prevent a nested for loop (o(n ^2), there is a problem in that what about the first element?
+# Would a later element be troublesome? if it has a lower count? 
+
+
+
+        
